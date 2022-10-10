29bc11e1bcef4e2c12f0a5713ab29bba12b45061
KashbullinRail <kashbullinrail@gmail.com>
2022-10-10 22:29:05 +0300

    организована передача интента от activity_main.xml к activity_second.xml
    передача text second to main

--- app/src/main/java/com/example/two_activity_phonenumber/MainActivity.kt
+++ app/src/main/java/com/example/two_activity_phonenumber/MainActivity.kt
@@ -1,19 +1,46 @@
 package com.example.two_activity_phonenumber
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val SECOND_ACTIVITY_DATA = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tvStartAction_2 = findViewById<TextView>(R.id.tvStartAction_2)
+        val btnGoActivityTwo = findViewById<Button>(R.id.btnGoActivityTwo)
 
-        tvStartAction_2.setOnClickListener{
-            startActivity(Intent(this, SecondActivity::class.java))
+        btnGoActivityTwo.setOnClickListener() {
+            startActivityForResult(Intent(this, SecondActivity::class.java),
+                SECOND_ACTIVITY_DATA
+            )
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+
+        val etStartAction_2 = findViewById<EditText>(R.id.etStartAction_2)
+        when (requestCode) {
+            SECOND_ACTIVITY_DATA -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    Log.d("TAG 1", "${data?.getStringExtra("$etStartAction_2")}")
+                    etStartAction_2.setText(data?.getStringExtra("phoneNumber"))
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/two_activity_phonenumber/SecondActivity.kt
+++ app/src/main/java/com/example/two_activity_phonenumber/SecondActivity.kt
@@ -1,11 +1,27 @@
 package com.example.two_activity_phonenumber
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
 import androidx.appcompat.app.AppCompatActivity
 
-class SecondActivity: AppCompatActivity(R.layout.activity_second) {
+class SecondActivity : AppCompatActivity(R.layout.activity_second) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val etPutPhoneNumber = findViewById<EditText>(R.id.etPutPhoneNumber)
+        val btnGoPhone = findViewById<Button>(R.id.btnGoPhone)
+
+        btnGoPhone.setOnClickListener {
+            setResult(Activity.RESULT_OK, Intent().apply {
+                putExtra("phoneNumber", "${etPutPhoneNumber.text}")
+            }
+            )
+            finish()
+        }
     }
 }
