b14249d56e3f29bbbdb6ae6d2a61680305e6032e
KashbullinRail <kashbullinrail@gmail.com>
2022-10-11 10:39:40 +0300

    добавлена фильтрация номеров по регуларным выражениям добавлена функция вызова звонка

--- app/src/main/java/com/example/two_activity_phonenumber/MainActivity.kt
+++ app/src/main/java/com/example/two_activity_phonenumber/MainActivity.kt
@@ -5,21 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val etStartAction_2 = findViewById<EditText>(R.id.etStartAction_2)
+        val etEnteredPhoneNumber = findViewById<EditText>(R.id.etEnteredPhoneNumber)
         val btnGoActivityTwo = findViewById<Button>(R.id.btnGoActivityTwo)
 
-        btnGoActivityTwo.setOnClickListener{
-            val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra("phone", etStartAction_2.text.toString())
-            startActivity(intent)
+        etEnteredPhoneNumber.setOnClickListener {
+            etEnteredPhoneNumber.setText("")
         }
 
+        btnGoActivityTwo.setOnClickListener {
+            val r = Regex("^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}\$")
+            if (r.matches(etEnteredPhoneNumber.text.toString())) {
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("phoneNumber", etEnteredPhoneNumber.text.toString())
+                startActivity(intent)
+            } else Toast
+                .makeText(applicationContext, "номер набран не верно", Toast.LENGTH_LONG)
+                .show()
+        }
     }
-
 }
--- app/src/main/java/com/example/two_activity_phonenumber/SecondActivity.kt
+++ app/src/main/java/com/example/two_activity_phonenumber/SecondActivity.kt
@@ -1,24 +1,28 @@
 package com.example.two_activity_phonenumber
 
-import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class SecondActivity : AppCompatActivity(R.layout.activity_second) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val etPutPhoneNumber = findViewById<EditText>(R.id.etPutPhoneNumber)
-        var phone = "1111111"
-
-        phone = intent.getStringExtra("phone").toString()
+        val etPutPhoneNumber = findViewById<TextView>(R.id.etPutPhoneNumber)
+        val btnGoPhoneCall = findViewById<Button>(R.id.btnGoPhoneCall)
+        val phone = intent.getStringExtra("phoneNumber").toString()
 
         etPutPhoneNumber.setText(phone)
 
+        btnGoPhoneCall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel: + ${etPutPhoneNumber.text.toString()}")
+            startActivity(intent)
+        }
     }
 }
